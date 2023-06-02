5dc6a68e41e38800ff154c195a65a60f9856bdf5
TheShyDev-Yoogo <yoogo.production@gmail.com>
2023-06-01 20:36:08 -0400

    restructuration folders

--- prevision/API/test.py
+++ prevision/API/test.py
@@ -1,9 +1,12 @@
-import os
+
 import pandas as pd
 
 
 attendancePath="affluence.csv"
 prevSellsPath="../data_vente.csv"
+meteoPath="../archive.csv"
 
-df1=pd.read_csv(attendancePath)[['date', 'phq_attendance_stats_sum']]
-df2=pd.read_csv(prevSellsPath)
+attendanceDf=pd.read_csv(attendancePath)[['date', 'phq_attendance_stats_sum']]
+prevSellsDf=pd.read_csv(prevSellsPath,sep=';')[['date', 'vente']]
+meteoDf=pd.read_csv(meteoPath)
+
