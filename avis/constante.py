5f36d7edd55bd4ef5c544cf516da117aa7e85e72
RenardBlanc <baktacheilyas@gmail.com>
2023-06-17 02:44:16 -0400

    correction erreur et test feature xg_boost

---
+++ avis/constante.py
@@ -0,0 +1,52 @@
+concurrents_place_des_arts = [
+    {
+        "nom": "La Cage Brasserie sportive",
+        "adresse": "150 Saint-Catherine St W, Montreal, QC H5B 1E9, Canada",
+        "lien_google_maps": "https://www.google.com/maps/place/La+Cage+%E2%80%93+Brasserie+sportive"
+    },
+    {
+        "nom": "Bâton Rouge",
+        "adresse": "180 Rue Sainte-Catherine Ouest, Montreal, QC H5B 1B5, Canada",
+        "lien_google_maps": "https://www.google.com/maps/place/B%C3%A2ton+Rouge+Steakhouse+%26+Bar"
+    },
+    {
+        "nom": "Eggspectation",
+        "adresse": "190 Saint Catherine Street West, Montreal, QC H2X 2X3, Canada",
+        "lien_google_maps": "https://www.google.com/maps/place/Eggspectation+Complex+Desjardins"
+    },
+    {
+        "nom": "St-Hubert",
+        "adresse": "100 Rue Sainte-Catherine Ouest, Montréal, QC, Canada",
+        "lien_google_maps": "https://www.google.com/maps/place/St-Hubert+Express"
+    },
+    {
+        "nom": "McDonald's",
+        "adresse": "150 Saint-Catherine St W, Montreal, QC H5B 1E9, Canada",
+        "lien_google_maps": "https://www.google.com/maps/place/McDonald's"
+    },
+    {
+        "nom": "Mikes",
+        "adresse": "Complexe Desjardins, Montreal, QC, Canada",
+        "lien_google_maps": "https://www.google.com/maps/place/Mikes"
+    },
+    {
+        "nom": "NYK'S",
+        "adresse": "Quartier des spectacles, Montreal, QC, Canada",
+        "lien_google_maps": "https://www.google.com/maps/place/NYK'S"
+    },
+    {
+        "nom": "Sésame",
+        "adresse": "Quartier des spectacles, Montreal, QC, Canada",
+        "lien_google_maps": "https://www.google.com/maps/place/S%C3%A9same"
+    },
+    {
+        "nom": "Les 3 Brasseurs",
+        "adresse": "1658 St Denis St, Montreal, QC H2X 3K6, Canada",
+        "lien_google_maps": "https://www.google.com/maps/place/Les+3+Brasseurs"
+    },
+    {
+        "nom": "Furco",
+        "adresse": "425 Mayor St, Montreal, QC H3A, Canada",
+        "lien_google_maps": "https://www.google.com/maps/place/Furco"
+    }
+]
